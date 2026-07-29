f79ba004a6b50626379c32430108db01ff4df3ce
Julkar Naen Nahian <nahian048@gmail.com>
2026-07-29 17:53:59 +0600

    chore: update Homebrew cask to v1.14.1

--- Casks/reader-md.rb
+++ Casks/reader-md.rb
@@ -1,6 +1,6 @@
 cask "reader-md" do
-  version "1.14.0"
-  sha256 "ee8e3dbbe4080b49a4c91551c8002a4fa7a141337e98bc2aa46cecc893db6b01"
+  version "1.14.1"
+  sha256 "049cf0a2239e134660e3a5e97c909d7ca680f9e97ee9cd4d11b8e40f7326e78b"
 
   url "https://github.com/jnahian/reader.md/releases/download/v#{version}/Reader.md.dmg",
       verified: "github.com/jnahian/reader.md/"
